31b20c09f152a8df7804f1879f5a1a14fd31031c
glidea <740696441@qq.com>
2026-07-01 15:07:48 +0800

    misc update

--- src/frontend/extension/entrypoints/options/main.ts
+++
@@ -1,8 +0,0 @@
-import { mount } from 'svelte'
-import Options from './Options.svelte'
-
-const app = mount(Options, {
-	target: document.getElementById('app') as HTMLElement
-})
-
-export default app
--- src/frontend/extension/entrypoints/popup/main.ts
+++
@@ -1,8 +0,0 @@
-import { mount } from 'svelte'
-import Popup from './Popup.svelte'
-
-const app = mount(Popup, {
-	target: document.getElementById('app') as HTMLElement
-})
-
-export default app
